da387f8dcc10a53a654161b1fb75716c9ca2c573
Victor Garitskyu <victor_95_22@mail.ru>
2021-12-27 21:54:17 +0600

    Initial Commit

---
+++ ToDoDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoDemo
+//
+//  Created by Victor Garitskyu on 27.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Victor Garitskyu on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
